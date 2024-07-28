9e5649366c64cf4bfd55c044db9ee0353964419c
demicheo <c.demicheo@gmail.com>
2024-07-28 13:57:02 +0200

    add header & footer home

---
+++ footer-home.php
@@ -0,0 +1,43 @@
+
+<footer class="footer seccion-negra">
+     
+    <div class="separacion container-fluid d-flex flex-column align-items-center justify-content-center">
+        <div class="motivo mb-2"></div>
+        <a href="index.php" class="footer-logo my-4">Chispas</a>
+
+        <section class="menu-footer container mt-2 row justify-content-center align-items-start">
+            <nav class="profundiza d-flex flex-column col-4 mt-1 mb-3 justify-content-center align-items-center col-md-2">
+                <h6 class="footer-titulo align-self-start">Profundiza</h6>
+                <ul class="col-12">
+                    <li><a href="#" class=" link-footer">Ponentes</a></li>
+                    <li><a href="#" class="link-footer">Agenda</a></li>
+                    <li><a href="#" class="link-footer">Equipo</a></li>
+                    <li><a href="#" class="link-footer">Prensa</a></li>
+                    <li><a href="#" class="link-footer">FAQs</a></li>
+                </ul>
+            </nav>
+            <nav class="conecta  d-flex flex-column col-4 mt-1 mb-3 justify-content-center align-items-center col-md-2">
+                <h6 class="footer-titulo align-self-start">Conecta</h6>
+                <ul class="col-12">
+                    <li><a href="https://www.linkedin.com/in/delademicheo/" class="link-footer">Linkedin</a></li>
+                    <li><a href="https://www.instagram.com/demicheo/" class="link-footer">Instagram</a></li>
+                    <li><a href="#" class="link-footer">Discord</a></li>
+                    <li><a href="#" class="link-footer">Youtube</a></li>
+                </ul>
+            </nav>
+        </section>
+    </div>
+    <div class="separacion container-fluid seccion-morada">
+        <section class="derechos container">
+            <p class="pt-4">Todos los derechos reservados. Chispas Copyright © 2024</p>
+        </section>
+    </div>
+ 
+</footer>
+
+ <!-- script -->
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="js/main.js"></script>
+</body>
+
+</html>
